efe3d15db123c2e7439d1dda7ad6bd735f472ffa
Matt Huston <hustonapps@gmail.com>
2020-03-31 13:06:31 -0600

    initial commit

---
+++ src/pages/Home/HomeClass.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export default class Home extends Component {
+  state = {};
+}
---
+++ src/pages/Home/HomeStateless.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Home = () => (
+  <div>
+    <h1>Marvel Movie Database (stateless)</h1>
+  </div>
+);
+
+// Home.propTypes = {
+//   : PropTypes.
+// };
+
+export default Home;
